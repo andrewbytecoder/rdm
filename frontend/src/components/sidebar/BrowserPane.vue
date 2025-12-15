a0468887a33e4065d2328318bdacf0287070e959
wangyazhou <564631192@qq.com>
2025-12-15 20:38:57 +0800

    feat(frontend): 移除浏览器面板中的服务器名称显示
    - 删除了 BrowserPane 组件中冗余的服务器名称文本显示
    - 简化了导航面板底部的过滤器控制逻辑
    - 优化了组件模板结构，提升可读性

--- frontend/src/components/sidebar/BrowserPane.vue
+++ frontend/src/components/sidebar/BrowserPane.vue
@@ -81,9 +81,6 @@ const filterTypeOptions = computed(() => {
 <template>
     <div class="nav-pane-container flex-box-v">
         <BrowserTree :server="currentName" />
-        <n-text>
-          {{ currentName }}
-        </n-text>
 <!--       通过点击图标，来控制是否显示过滤器-->
         <div class="nav-pane-bottom flex-box-h" v-if="filterForm.showFilter">
             <n-input-group>
